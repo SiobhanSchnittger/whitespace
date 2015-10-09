bd1fa078fec9e1d760d3ed72963860b767127974
Siobhan <Siobhan.Schnittger@gmail.com>
2015-10-09 13:22:26 +0100

    Updated CSS / Header
    - Aligned Read More buttons properly under posters
    - Uploaded images from Pyg FB page & added width: 100% on slider
    - Created and uploaded toggle menu icon (to be implemented)
    - Updated footer styles as per comments
    - Changed footer font to Lato

--- mixedmedia/header.php
+++ mixedmedia/header.php
@@ -37,7 +37,7 @@
 		</div><!-- .site-branding -->
 		<div class="nav-wrap">
             <nav id="site-navigation" class="main-navigation" role="navigation">
-                <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'mixedmedia' ); ?></button>
+                <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( '...', 'mixedmedia' ); ?></button>
                 <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
             </nav><!-- #site-navigation -->
         </div><!-- #nav-wrap -->
